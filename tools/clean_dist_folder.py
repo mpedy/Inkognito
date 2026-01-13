e529f63c8af9ae2292f6f27990aaa7fa27e489be
Matteo <matteo.provendola@unige.it>
2026-01-13 15:01:35 +0100

    More compacted solution

--- tools/clean_dist_folder.py
+++ tools/clean_dist_folder.py
@@ -1,15 +1,8 @@
 import os
+import shutil
 
 #clean all files and folders in static/dist
 dist_folder = "static/dist"
-for filename in os.listdir(dist_folder):
-    file_path = os.path.join(dist_folder, filename)
-    try:
-        if os.path.isfile(file_path) or os.path.islink(file_path):
-            os.unlink(file_path)
-        elif os.path.isdir(file_path):
-            import shutil
-            shutil.rmtree(file_path)
-    except Exception as e:
-        print(f'Failed to delete {file_path}. Reason: {e}')
+shutil.rmtree(dist_folder, ignore_errors=True)
 print(f'Cleaned {dist_folder} folder.')
+os.makedirs(dist_folder, exist_ok=True)
